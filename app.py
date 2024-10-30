d08b62dbf595b3c6b1acf540f528eb88bc68d8fd
Andrea Shobe <20andrea03@gmail.com>
2024-10-29 20:08:25 -0500

    Update app.py
    Project 3

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from shinywidgets import render_plotly, output_widget, render_widget
 from shiny.express import input, ui, render
 from palmerpenguins import load_penguins
 import seaborn as sns
-from shiny import render, reactive
+from shiny import render, reactive, req
 
 ui.page_opts(title="Filling layout", fillable=True)
 
@@ -15,11 +15,21 @@ ui.page_opts(title="Andrea's Penguin Playground", fillable=False)
 with ui.sidebar(open="open"):
     ui.h2("Sidebar")
 
-    ui.input_selectize("selected_attribute","Select Attributes",
+    
+    ui.input_selectize("selected_x_attribute","Select X Attribute",
+                       ["bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g"])
+
+    ui.input_selectize("selected_y_attribute","Select Y Attribute",
                        ["bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g"])
     
-    ui.input_numeric("plotly_bin_count","Ploty Bins",1,min=1,max=20)
+    ui.input_numeric("plotly_bin_count","Ploty Bins",10,min=1,max=20)
     ui.input_slider("seaborn_bin_count","Seaborn Bins",1,20,10)
+
+    ui.input_checkbox_group("selected_island","Select Island",
+                      ["Torgersen","Dream","Biscoe"],
+                      selected=["Torgersen","Dream"],inline=False)
+                
+    
     ui.input_checkbox_group("selected_species_list","Choose Species",
                            ["Adelie", "Gentoo","Chinstrap"],
                            selected=["Adelie","Gentoo"],
@@ -33,31 +43,33 @@ with ui.layout_columns():
         "Penguins Data Table"
         @render.data_frame
         def penguinstable_df():
-            return render.DataTable(filtered_data(), filters=True,selection_mode='row')
+            return render.DataTable(filtered_data(),selection_mode='row')
         
 
     with ui.card():
         "Penguins Data Grid"
         @render.data_frame
         def penguinsgrid_df():
-            return render.DataGrid(filtered_data(), filters=True, selection_mode="row")
+            return render.DataGrid(filtered_island(),selection_mode="row")
 
 
 with ui.layout_columns():
     with ui.card():
         @render.plot(alt="A Seaborn Histogram")
         def plot():
-            ax=sns.histplot(data=filtered_data(),x="body_mass_g",bins=input.seaborn_bin_count())
-            ax.set_title("Palmer Penguins")
-            ax.set_xlabel("Mass (g)")
-            ax.set_ylabel("Count")
-            return ax
+            df=penguins
+            df[input.selected_x_attribute()].hist(grid=False)
+            plt=sns.histplot(data=filtered_data(),x="body_mass_g",bins=input.seaborn_bin_count())
+            plt.set_title("Palmer Penguins")
+            plt.set_xlabel(input.selected_x_attribute())
+            plt.set_ylabel("Count")
+            return plt
             
     with ui.card():
         "Plotly Histogram"
         @render_plotly
         def plotlyhistogram():
-            return px.histogram(filtered_data(),x=input.selected_attribute(),
+            return px.histogram(filtered_data(),x=input.selected_x_attribute(),
                                nbins=input.plotly_bin_count(), color="species").update_layout(
                 xaxis_title="Bill Length (mm)",
                 yaxis_title="Count",)
@@ -68,22 +80,25 @@ with ui.card(full_screen=True):
     def plotly_scatterplot():
         return px.scatter(
             data_frame=filtered_data(),
-            x="body_mass_g",
-            y="bill_depth_mm",
+            x=input.selected_x_attribute(),
+            y=input.selected_y_attribute(),
             color="species",
             color_discrete_sequence=["red","orange","blue"],
             labels={"body_mass_g": "Body Mass (g)",
-                   "bill_depth_mm":"Bil Depth (mm):"})
-
-# --------------------------------------------------------
-# Reactive calculations and effects
-# --------------------------------------------------------
-
-# Add a reactive calculation to filter the data
-# By decorating the function with @reactive, we can use the function to filter the data
-# The function will be called whenever an input functions used to generate that output changes.
-# Any output that depends on the reactive function (e.g., filtered_data()) will be updated when the data changes.
+                   "bill_depth_mm":"Bill Depth (mm):",
+                   "flipper_length_mm":"Flipper Length (mm)",
+                    "bill_length_mm":"Bill Length (mm)"},)
 
 @reactive.calc
 def filtered_data():
-    return penguins
+    req(input.selected_species_list())
+    isSpeciesMatch=penguins["species"].isin(input.selected_species_list())
+    return penguins[isSpeciesMatch]
+
+def filtered_island():
+    req(input.selected_island())
+    isIslandMatch=penguins["island"].isin(input.selected_island())
+    return penguins[isIslandMatch]
+    
+    
+
